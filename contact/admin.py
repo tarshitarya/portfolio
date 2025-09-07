a8a789a0eea7b25cb2c3e5f7920e6093eae1f814
tarshitarya <aryatarshit@gmail.com>
2025-09-07 17:04:22 +0530

    update contact page

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+admin.site.register(Contact)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from .models import Contact
+from .forms import ContactForm
 
-# Create your views here.
 def contact(request):
-    return render(request, 'contact/contact.html')
+    user_name = None
 
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save()
+            user_name = contact.name
+            return render(request, 'contact/contact.html', {
+                'form': ContactForm(),  # blank form
+                'Contacts': Contact.objects.all(),
+                'success': True,
+                'user_name': user_name
+            })
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact/contact.html', {
+        'form': form,
+        'Contacts': Contact.objects.all()
+    })
